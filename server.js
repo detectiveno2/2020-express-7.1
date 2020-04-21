cfe92fd0348690eb89eb98fe2253381ae63d7efe
Glitch (hello-express) <none>
2020-04-21 04:11:54 +0000

    😄👼 Checkpoint ./server.js:15235691/683 ./views/index.pug:15235691/52 ./package.json:15235691/1279 ./views/todos/index.pug:15235691/41

--- server.js
+++ server.js
@@ -6,10 +6,24 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 // https://expressjs.com/en/starter/basic-routing.html
-app.get('/', (request, response) => {
-  response.send('I love CodersX');
+app.get('/', (req, res) => {
+  res.render('index');
 });
+
+app.get('/todos', (req, res) => {
+  res.render('todos/index', {
+    todos: [
+      {id: 1, name: 'Đi chợ'},
+      {id: 2, name: 'Nấu ăn'},
+      {id: 3, name: 'Giặt đồ'},
+      {id: 4, name: 'Học code'},
+    ],
+  })
+})
 
 // listen for requests :)
 app.listen(process.env.PORT, () => {
