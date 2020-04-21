35fa40ab79c60ad795226db3280b7cf47d65f864
Glitch (hello-express) <none>
2020-04-21 08:26:05 +0000

    👙⛽️ Checkpoint ./server.js:15235691/5285 ./views/todos/index.pug:15235691/126 ./package.json:15235691/1330

--- server.js
+++ server.js
@@ -3,40 +3,51 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require('express');
+const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser");
+
 var todos = [
-      {id: 1, name: 'Đi chợ'},
-      {id: 2, name: 'Nấu ăn'},
-      {id: 3, name: 'Giặt đồ'},
-      {id: 4, name: 'Học code'},
+  { name: "Đi chợ" },
+  { name: "Nấu ăn" },
+  { name: "Giặt đồ" },
+  { name: "Học code" }
 ];
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "pug");
+app.set("views", "./views");
 
 // https://expressjs.com/en/starter/basic-routing.html
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/todos', (req, res) => {
-  res.render('todos/index', {
-    todos: todos,
-  })
-})
+app.get("/todos", (req, res) => {
+  res.render("todos/index", {
+    todos: todos
+  });
+});
 
-app.get('/todos/search', (req, res) => {
+app.get("/todos/search", (req, res) => {
   let q = req.query.q;
   let matchedTodos = todos.filter(todo => {
     return todo.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-  })
-  res.render('todos/index', {
+  });
+  res.render("/todos/index", {
     todos: matchedTodos,
     queryInput: q
-  })
-})
+  });
+});
+
+app.post("/todos/create", (req, res) => {
+  let newTodo = req.body;
+  todos.push(newTodo);
+  res.redirect("back");
+});
 
 // listen for requests :)
 app.listen(process.env.PORT, () => {
